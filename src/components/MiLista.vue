998a0a82cac41ee80385029af43d9ffe77a56c09
cristinaraipan <cristina.raipanc@gmail.com>
2020-07-24 18:29:11 -0400

    se modifica MiLista, se agregan enlaces en los li y en el array de script

--- src/components/MiLista.vue
+++ src/components/MiLista.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="hello">
-      <h1>{{msg}}</h1>
-        <h3>Tech</h3>
-      <ul>
-        <li v-for= "cosa in tech" v-bind:key= "cosa.name">{{cosa.name}}</li>
-      </ul>
-      <h3>Financial</h3>
-      <ul>
-        <li v-for= "cosa in financial" :key= "cosa.name">{{cosa.name}}</li>
-      </ul>
-    
+    <h1>{{msg}}</h1>
+    <h3>Tech</h3>
+    <ul>
+      <li v-for="cosa in tech" v-bind:key="cosa.name">
+        <a target="_blank" v-bind:href="cosa.enlace">{{ cosa.name }}</a>
+      </li>
+    </ul>
+    <h3>Financial</h3>
+    <ul>
+      <li v-for="cosa in financial" :key="cosa.name">
+        <a target="_blank" v-bind:href="cosa.enlace">{{ cosa.name }}</a>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -19,37 +22,43 @@ export default {
   props: {
     msg: String,
   },
-  data(){
-    return{
-    tech: [
-            {
-                name: "HTML",
-            },
-            {
-                name: "CSS",
-            },
-            {
-                name: "Vue",
-            },
-            {
-                name: "Firebase",
-            },
-            
-        ],
-        financial: [
-            {
-                name: "investing"
-            },
-            {
-                name: "budgeting"
-            },
-            {
-                name: "balancing a checkbook"
-            }
-        ]
-    }
-  }
-}
+  data() {
+    return {
+      tech: [
+        {
+          name: "HTML",
+          enlace: "https://www.w3schools.com/html/",
+        },
+        {
+          name: "CSS",
+          enlace: "https://www.w3schools.com/html/html_css.asp",
+        },
+        {
+          name: "Vue",
+          enlace: "https://vuejs.org",
+        },
+        {
+          name: "Firebase",
+          enlace: "https://firebase.google.com",
+        },
+      ],
+      financial: [
+        {
+          name: "investing",
+          enlace: "https://es.investing.com"
+        },
+        {
+          name: "budgeting",
+          enlace: "https://www.mymoneycoach.ca/budgeting/what-is-a-budget-planning-forecasting"
+        },
+        {
+          name: "balancing a checkbook",
+          enlace: "https://www.linguee.com/english-spanish/translation/balance+a+checkbook.html"
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -59,7 +68,7 @@ h3 {
 }
 ul {
   list-style-type: none;
-  
+
   padding: 0;
 }
 li {
